2dd9e4ffb7f629dd53d3c75e226117e9ab84ddcd
Edderic Ugaddan <eugaddan@panoramaed.com>
2020-10-11 19:35:58 -0400

    create project controller

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,4 +1,28 @@
 class ProjectController < ApplicationController
   def main
   end
+
+  def save
+    project_id = params['project_id']
+
+    if project_id
+      project = Project.find(uuid: project_id)
+      project.update(
+        data: params['project_data']
+      )
+
+      render :json, {
+        message: 'Successfully updated project'
+      }
+    else
+      Project.create(
+        uuid: project_id,
+        data: params['project_data']
+      )
+
+      render :json, {
+        message: 'Successfully created project!'
+      }
+    end
+  end
 end
