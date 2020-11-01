d716ad32768fd0558b8a20c769769c70a1e4420a
Edderic Ugaddan <eugaddan@panoramaed.com>
2020-11-01 10:18:36 -0500

    update devise settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  require 'action_mailer/railtie'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
